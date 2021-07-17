47dba2565da3e2f6bfc464a773a5085f36053fa4
Sai-boop-hub <85127781+Sai-boop-hub@users.noreply.github.com>
2021-07-16 22:11:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function enter() {
+
+window.location = ("recipe.html");
+
+}
+
+
+function egg() {
+
+window.location = ("egg.html");
+
+}
+
+function fries() {
+
+window.location = ("fries.html");
+
+}
+
+function juice() {
+
+window.location = ("juice.html");
+
+}
+
+function milk() {
+
+window.location = ("milk.html");
+
+}
+
+function milkshake() {
+
+window.location = ("milkshake.html");
+
+}
+
+function salad() {
+
+window.location = ("salad.html");
+
+}
+
+function Back() {
+
+window.location = ("recipe.html");
+
+}
+
+function back() {
+
+window.location = ("index.html");
+
+}
